eb76aeae2a788e22c8ef0c6ac9cd281abfcc21b7
Denis Olsem <dols3m@gmail.com>
2017-12-03 07:02:02 -0500

    Changed package name again.

--- mylib/__init__.py
+++
@@ -1,1 +0,0 @@
-from .rsizeof import rsizeof
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     author="Denis Semenenko",
     author_email="dols3m@gmail.com",
-    packages=["dols3m-lib"],
+    packages=["dols3mlib"],
     url="https://github.com/dolsem/python-lib",
     description="Personal collection of useful python code.",
 )
