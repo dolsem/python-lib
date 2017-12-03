4d04fa0b2d025f89860c84f060cc473b43277194
Denis Olsem <dols3m@gmail.com>
2017-12-03 06:56:03 -0500

    Renamed package to dols3m-lib, added distribution files.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="dols3m-lib",
+    version="0.1.0",
+    author="Denis Semenenko",
+    author_email="dols3m@gmail.com",
+    packages=["dols3m-lib"],
+    url="https://github.com/dolsem/python-lib",
+    description="Personal collection of useful python code.",
+)
